7d1ed9fea70f0e4848c907f76b14fe723c26fed5
KonradPolarczyk <konradpolarczyk98@gmail.com>
2020-08-10 20:16:48 +0200

    Camera movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class CameraController : MonoBehaviour
+    {
+        public int moveCameraThreshold = 50;
+        public float moveSpeed = 1f;
+        private Camera cam;
+
+        private void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            var moveDirection = GetMoveDirection();
+
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
+
+        private Vector3 GetMoveDirection()
+        {
+            Vector3 direction = Vector3.zero;
+
+            if (Input.mousePosition.x < moveCameraThreshold)
+            {
+                direction += Vector3.left;
+            }
+
+            if (Input.mousePosition.y < moveCameraThreshold)
+            {
+                direction += Vector3.down;
+            }
+
+            if (Input.mousePosition.x > cam.pixelWidth - moveCameraThreshold)
+            {
+                direction += Vector3.right;
+            }
+
+            if (Input.mousePosition.y > cam.pixelHeight - moveCameraThreshold)
+            {
+                direction += Vector3.up;
+            }
+
+            return direction;
+        }
+    }
+}
